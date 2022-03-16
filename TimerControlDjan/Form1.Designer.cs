143b28ac675a71ccbbb6077403c18a8d2142e26e
JanssenDaan <78667894+JanssenDaan@users.noreply.github.com>
2022-03-16 11:39:40 +0100

    Add project files.

---
+++ TimerControlDjan/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TimerControlDjan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddTimerDjan = new System.Windows.Forms.Button();
+            this.Flow = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnAddTimerDjan
+            // 
+            this.btnAddTimerDjan.Location = new System.Drawing.Point(13, 13);
+            this.btnAddTimerDjan.Name = "btnAddTimerDjan";
+            this.btnAddTimerDjan.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTimerDjan.TabIndex = 1;
+            this.btnAddTimerDjan.Text = "Add Timer";
+            this.btnAddTimerDjan.UseVisualStyleBackColor = true;
+            this.btnAddTimerDjan.Click += new System.EventHandler(this.btnAddTimerDjan_Click);
+            // 
+            // Flow
+            // 
+            this.Flow.Location = new System.Drawing.Point(134, 12);
+            this.Flow.Name = "Flow";
+            this.Flow.Size = new System.Drawing.Size(654, 426);
+            this.Flow.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Flow);
+            this.Controls.Add(this.btnAddTimerDjan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAddTimerDjan;
+        private System.Windows.Forms.FlowLayoutPanel Flow;
+    }
+}
+
---
+++ TimerControlDjan/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DjanControls;
+using DjanDLL;
+
+namespace TimerControlDjan
+{
+    public partial class Form1 : Form
+    {
+
+        UC1 UCTimerControlDjan = new UC1();
+        public Helpers Helpers = new Helpers();
+        public List<UC1> UC1List = new List<UC1>();
+        public int NrOfControls;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int r = Helpers.RandomNumber(1, 10);
+        }
+
+        private void btnAddTimerDjan_Click(object sender, EventArgs e)
+        {
+            NrOfControls++;
+
+            var myControl = new UC1();
+            myControl.Nr = NrOfControls;
+            myControl.lblOneDjan.Text = NrOfControls.ToString();
+            Flow.Controls.Add(myControl);
+            Flow.AutoScroll = true;
+            
+        }
+
+        private void printPreviewControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlOneDjan_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
